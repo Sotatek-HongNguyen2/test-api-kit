7e067e9fd87e7505cbc2a6dbc0e967a8cf973807
Pink Cheetah <t2210545@partner.coway.com>
2024-06-19 09:39:38 +0700

    fix: update design

--- src/components/organisms/wil-tabs/will-type-modal/index.tsx
+++ src/components/organisms/wil-tabs/will-type-modal/index.tsx
@@ -1,16 +1,26 @@
-import WillModal from "@/components/atoms/modal"
-import "./styles.scss"
-import { AppButton } from "@/components/atoms/button"
-import { Text } from "@/components/atoms/text"
-import { PlusOutlined } from "@ant-design/icons"
-import { Flex } from "antd"
-import useDisclosure from "@/hooks/useDisclosure"
-import { useState } from "react"
-import { WillType } from "@/types"
-import { DestructionIcon, ForwardingIcon, InheritanceIcon } from "@/assets/icons/custom-icon"
-import { WillTypeCard } from "@/components/molecules/will-type-card"
-import { useNavigate } from "react-router-dom"
-import { APP_ROUTES_PATHS } from "@/constants"
+import WillModal from "@/components/atoms/modal";
+import "./styles.scss";
+import { AppButton } from "@/components/atoms/button";
+import { Text } from "@/components/atoms/text";
+
+import { PlusOutlined } from "@ant-design/icons";
+import { Col, Flex, Row } from "antd";
+
+import useDisclosure from "@/hooks/useDisclosure";
+
+import { useState } from "react";
+
+import { WillType } from "@/types";
+import {
+  DestructionIcon,
+  ForwardingIcon,
+  InheritanceIcon,
+} from "@/assets/icons/custom-icon";
+import { WillTypeCard } from "@/components/molecules/will-type-card";
+
+import { useNavigate } from "react-router-dom";
+
+import { APP_ROUTES_PATHS } from "@/constants";
 
 export interface WillTypeItem {
   icon: React.ReactNode;
@@ -23,23 +33,25 @@ const willTypeItems: WillTypeItem[] = [
   {
     icon: <InheritanceIcon />,
     title: "Inheritance",
-    description: "When activated, a designated number of signatures will be required for beneficiaries to access the wallet.",
-    type: "inheritance"
+    description:
+      "When activated, a designated number of signatures will be required for beneficiaries to access the wallet.",
+    type: "inheritance",
   },
   {
     icon: <ForwardingIcon />,
     title: "Forwarding",
-    description: "When activated, assets designated by the owner will be sent directly to the public addresses of listed beneficiaries.",
-    type: "forwarding"
+    description:
+      "When activated, assets designated by the owner will be sent directly to the public addresses of listed beneficiaries.",
+    type: "forwarding",
   },
   {
     icon: <DestructionIcon />,
     title: "Destruction",
-    description: "When activated, all assets will be sent to the Ethereum genesis address, thus destroyed permanently.",
-    type: "destruction"
-  }
-
-]
+    description:
+      "When activated, all assets will be sent to the Ethereum genesis address, thus destroyed permanently.",
+    type: "destruction",
+  },
+];
 
 export const WillTypeModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -52,39 +64,58 @@ export const WillTypeModal = () => {
   };
 
   return (
-    <>
-      <AppButton type="primary" size="xl" icon={<PlusOutlined />} onClick={onOpen}>
-        <Text size="text-lg" className="uppercase font-bold" >Create a Will</Text>
+    <div className="will-tab-modal">
+      <AppButton
+        type="primary"
+        size="xl"
+        icon={<PlusOutlined />}
+        onClick={onOpen}
+      >
+        <Text size="text-lg" className="uppercase font-bold">
+          Create a Will
+        </Text>
       </AppButton>
       <WillModal
+        width={672}
         open={isOpen}
         handleCancel={onClose}
-        title={<Text size="text-3xl" className="font-bold neutral-1">Choose Will Type</Text>}
+        title={
+          <Text size="text-3xl" className="font-bold neutral-1">
+            Choose Will Type
+          </Text>
+        }
         hideFooter
       >
         <Flex vertical gap={24}>
-          <Flex gap={16} align="center">
-            {
-              willTypeItems?.map((item, index) => (
-                <WillTypeCard
-                  key={`${item?.type}-${index}`}
-                  {...item}
-                  active={willType === item?.type}
-                  onClick={() => setWillType(item?.type)}
-                />
-              ))
-            }
+          <Flex gap={24} align="center">
+            {willTypeItems?.map((item, index) => (
+              <WillTypeCard
+                key={`${item?.type}-${index}`}
+                {...item}
+                active={willType === item?.type}
+                onClick={() => setWillType(item?.type)}
+              />
+            ))}
           </Flex>
-          <Flex justify="space-between" align="center">
-            <AppButton size="xl" onClick={onClose}>
-              <Text size="text-lg" className="neutral-1 font-bold uppercase">Cancel</Text>
-            </AppButton>
-            <AppButton size="xl" type="primary" onClick={createAWill}>
-              <Text size="text-lg" className="neutral-3 font-bold uppercase">create a will</Text>
-            </AppButton>
-          </Flex>
+
+          <Row gutter={24}>
+            <Col span={12}>
+              <AppButton block size="xl" onClick={onClose}>
+                <Text size="text-lg" className="neutral-1 font-bold uppercase">
+                  Cancel
+                </Text>
+              </AppButton>
+            </Col>
+            <Col span={12}>
+              <AppButton block size="xl" type="primary" onClick={createAWill}>
+                <Text size="text-lg" className="neutral-3 font-bold uppercase">
+                  create a will
+                </Text>
+              </AppButton>
+            </Col>
+          </Row>
         </Flex>
       </WillModal>
-    </>
-  )
-}
+    </div>
+  );
+};
