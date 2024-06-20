32f94abc12ce2331ad4f8af9754e33b23264f91d
Pink Cheetah <t2210545@partner.coway.com>
2024-06-20 19:47:37 +0700

    fix: fix bug

--- src/components/molecules/asset-item/index.tsx
+++ src/components/molecules/asset-item/index.tsx
@@ -3,8 +3,8 @@ import { Flex } from "antd";
 
 import { Text } from "@/components/atoms/text";
 import { AssetData, ItemOwnerBalance } from "@/types";
-import useFormattedNumber from "@/hooks/useFormatToken";
-import { getWalletSlice, useAppSelector } from "@/store";
+// import useFormattedNumber from "@/hooks/useFormatToken";
+// import { getWalletSlice, useAppSelector } from "@/store";
 
 import { AssetName } from "./AssetName";
 
@@ -13,24 +13,41 @@ interface AssetItemProps {
   ownerBalance: ItemOwnerBalance[];
 }
 export const AssetItem = ({ asset, ownerBalance }: AssetItemProps) => {
-  const { balance } = useAppSelector(getWalletSlice);
+  // const { balance } = useAppSelector(getWalletSlice);
 
-  const formattedAmount = useFormattedNumber(Number(asset?.amount || 0));
-  const str = useFormattedNumber(Number(balance));
+  const formatNumber = (number: number): string => {
+    const numberStr = number.toString();
+    const parts = numberStr.split(".");
+    let integerPart = parts[0];
+    let decimalPart = parts[1] || "00";
+    decimalPart = decimalPart.substring(0, 2);
+    integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return `${integerPart}.${decimalPart}`;
+  };
+
+  // const formattedAmount = useFormattedNumber(Number(asset?.amount || 0));
+  // const str = useFormattedNumber(Number(balance));
   const getAmount = () => {
-    console.log(formattedAmount, balance);
-    if (Number(asset?.amount) > Number(balance)) {
-      return str;
+    for (const item of ownerBalance ?? []) {
+      if (asset.asset === item.address) {
+        if (Number(asset?.amount) > Number(item.balance)) {
+          return formatNumber(Number(item.balance));
+        }
+        return formatNumber(Number(asset?.amount));
+      }
     }
-    return formattedAmount;
+    return "0.0";
+    // console.log(formattedAmount, balance);
+    // if (Number(asset?.amount) > Number(balance)) {
+    //   return str;
+    // }
+    // return formattedAmount;
   };
   return (
     <Flex align="center" justify="space-between">
       <AssetName ownerBalance={ownerBalance} asset={asset} />
       <Flex vertical>
-        <Text className="font-semibold neutral-1">
-          {asset.amount ? getAmount() : "0.0"}
-        </Text>
+        <Text className="font-semibold neutral-1">{getAmount()}</Text>
       </Flex>
     </Flex>
   );
