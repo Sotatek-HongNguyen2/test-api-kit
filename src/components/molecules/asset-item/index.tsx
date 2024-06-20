0c8860332022308ffb7302f508b32f4f09ea3d68
Pink Cheetah <t2210545@partner.coway.com>
2024-06-20 19:35:35 +0700

    fix bug

--- src/components/molecules/asset-item/index.tsx
+++ src/components/molecules/asset-item/index.tsx
@@ -4,6 +4,7 @@ import { Flex } from "antd";
 import { Text } from "@/components/atoms/text";
 import { AssetData, ItemOwnerBalance } from "@/types";
 import useFormattedNumber from "@/hooks/useFormatToken";
+import { getWalletSlice, useAppSelector } from "@/store";
 
 import { AssetName } from "./AssetName";
 
@@ -12,14 +13,23 @@ interface AssetItemProps {
   ownerBalance: ItemOwnerBalance[];
 }
 export const AssetItem = ({ asset, ownerBalance }: AssetItemProps) => {
-  const formattedAmount = useFormattedNumber(Number(asset?.amount || 0));
+  const { balance } = useAppSelector(getWalletSlice);
 
+  const formattedAmount = useFormattedNumber(Number(asset?.amount || 0));
+  const str = useFormattedNumber(Number(balance));
+  const getAmount = () => {
+    console.log(formattedAmount, balance);
+    if (Number(asset?.amount) > Number(balance)) {
+      return str;
+    }
+    return formattedAmount;
+  };
   return (
     <Flex align="center" justify="space-between">
       <AssetName ownerBalance={ownerBalance} asset={asset} />
       <Flex vertical>
         <Text className="font-semibold neutral-1">
-          {asset.amount ? formattedAmount : "0.0"}
+          {asset.amount ? getAmount() : "0.0"}
         </Text>
       </Flex>
     </Flex>
